1004206300ad71266d1d4efdd4d0586e0d493124
Vivek Pandey <117655085+SevenbitsVivek@users.noreply.github.com>
2023-12-17 21:11:43 +0530

    Made minor changes

--- SoulBoundTokenV2.sol
+++ SoulBoundTokenV2.sol
@@ -48,7 +48,7 @@ contract SoulBoundTokenV2 is ERC721, ERC721URIStorage, Ownable {
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId) override internal {
         if (totalNftMinted <= nftToBeMintInLockingPeriod) {
-            if (nftUser[from][tokenId].boughtTime + nftLockingPeriod >= block.timestamp || nftUser[to][tokenId].boughtTime + nftLockingPeriod >= block.timestamp) {
+            if (nftUser[from][tokenId].lockingPeriod >= block.timestamp || nftUser[to][tokenId].lockingPeriod >= block.timestamp) {
                 require(from == address(0) || to == address(0), "Not allowed to transfer nft");
             }
             nftUser[from][tokenId].isSold = true;
@@ -56,7 +56,7 @@ contract SoulBoundTokenV2 is ERC721, ERC721URIStorage, Ownable {
 
         if (totalNftMinted > nftToBeMintInLockingPeriod && totalNftMinted <= totalNftSupply) {
             if (nftUser[from][tokenId].tokenId <= nftToBeMintInLockingPeriod) {
-                require(nftUser[from][tokenId].boughtTime + nftLockingPeriod <= block.timestamp, "Not allowed to transfer nft");
+                require(nftUser[from][tokenId].lockingPeriod <= block.timestamp, "Not allowed to transfer nft");
             }
             nftUser[from][tokenId].isSold = true;
         }
